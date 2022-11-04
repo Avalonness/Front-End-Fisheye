e79946060cbf96b1c6587589d98db72659c8a7dd
Avalonnes <vincentdomingues22@hotmail.com>
2022-11-04 10:50:42 +0100

    Ajout des datas dynamique via fetch

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,31 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, country, city, tagline, price} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        //Ajoute l'image de profil
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        //Ajoute le nom
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        //AJoute la localisation
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = `${country}, ${city}`;
+        //Ajoute la phrase d'accroche
+        const p = document.createElement( 'p' );
+        p.textContent = `${tagline}`;
+        //Ajoute le prix
+        const span = document.createElement( 'span' );
+        span.textContent = `${price}€/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(p);
+        article.appendChild(span);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,45 +1,24 @@
-    async function getPhotographers() {
+   let photographers = []
+   
+   async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        fetch('https://raw.githubusercontent.com/OpenClassrooms-Student-Center/Front-End-Fisheye/main/data/photographers.json')
+            .then(res => res.json())
+            .then(data => setPhotographers(data.photographers) );
+
+        // Fonction qui va mettre en place les datas des photographes
+            function setPhotographers(data) {
+                //Cible la balise dans le DOM
+                const photographersSection = document.querySelector(".photographer_section");
+                photographers.push(data)
+
+                photographers[0].forEach((photographe)=>{
+                    const photographerModel = photographerFactory(photographe);
+                    const userCardDOM = photographerModel.getUserCardDOM();
+                    photographersSection.appendChild(userCardDOM);
+                })
+            }
         // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
     }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
-
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
-
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+    getPhotographers()
